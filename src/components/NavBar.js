0af82a8527dde0d57e0aa372bb37ad3b1dd46077
Ma-Rah <martin.lars.rahm@gmail.com>
2021-02-24 21:09:16 +0100

    gallery update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,27 +5,25 @@ import { SocialIcon } from "react-social-icons";
 export default function NavBar() {
 	return (
 		<header className="bg-gray-600">
-			<div className="container mx-auto flex flex-col justify-between min-w-screen p-3 md:flex-row text-2xl">
-				<nav className="flex flex-col md:flex-row ">
+			<div className="container mx-auto flex flex-col justify-between min-w-screen p-3 md:flex-row text-2xl ">
+				<nav className="flex flex-col text-base md:text-xl md:flex-row  ">
 					<NavLink
 						to="/"
 						exact
-						// className=" inline-flex items-center py-1 px-1 md:my-3 rounded text-white hover:text-blue-300 text-6xl"
-						// activeClassName="bg-red-700"
-						className="inline-flex items-center pb-3 px-3 mr-4 text-white hover:text-blue-300 text-4xl font-bold cursive tracking-widest text-5xl"
+						className="inline-flex items-center pb-2 px-3 mr-4 text-white hover:text-blue-300 text-5xl font-bold cursive tracking-widest"
 					>
 						Home
 					</NavLink>
 					<NavLink
 						to="/post"
-						className=" inline-flex items-center py-3 px-3 md:my-6 rounded text-white hover:text-blue-300 "
+						className=" inline-flex items-center py-2 px-3 md:my-6 rounded text-white hover:text-blue-300 "
 						activeClassName=" bg-red-700"
 					>
 						Blog posts
 					</NavLink>
 					<NavLink
 						to="/project"
-						className="inline-flex items-center py-3 px-3 md:my-6 rounded text-white hover:text-blue-300"
+						className="inline-flex items-center py-2 px-3 md:my-6 rounded text-white hover:text-blue-300"
 						activeClassName="bg-red-700"
 					>
 						Projects
