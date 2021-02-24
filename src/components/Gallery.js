35224a51c4cd42d638b443dcfbf0a73b02234940
Ma-Rah <martin.lars.rahm@gmail.com>
2021-02-24 22:39:47 +0100

    gallery update 2

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -20,21 +20,22 @@ export default function Gallery() {
 			.then((data) => setImage(data))
 			.catch(console.error);
 	}, []);
-	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+
+	if (/webOS|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 		return (
 			<main className="bg-white min-h-screen w-min-screen p-2">
 				<section className="">
 					<h1 className="text-5xl flex justify-center cursive">My Gallery</h1>
-					<h2 className="text-lg text-gray-600 flex justify-center mb-12">
-						Some of my favorite photos from recent years!
+					<h2 className="text-lg text-gray-600 flex justify-center mb-5">
+						Some of my favorite photos I've shot in recent years!
 					</h2>
-					<div className="gap-1 grid-flow-row-dense auto-rows-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 cursor-pointer  ">
+					<div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-1 grid-flow-rows-dense cursor-pointer  ">
 						{imageData &&
 							imageData.map((photo, index) => (
 								<img
 									src={photo.mainImage.asset.url}
 									alt=""
-									className=" object-cover h-full w-full "
+									className=" object-cover h-full w-full hover:transform-scale-105 "
 								/>
 							))}
 					</div>
@@ -46,17 +47,17 @@ export default function Gallery() {
 			<main className="bg-white min-h-screen w-min-screen p-2">
 				<section className="">
 					<h1 className="text-5xl flex justify-center cursive">My Gallery</h1>
-					<h2 className="text-lg text-gray-600 flex justify-center mb-12">
-						Some of my favorite photos from recent years!
+					<h2 className="text-lg text-gray-600 flex justify-center mb-5">
+						Some of my favorite photos I've shot in recent years!
 					</h2>
 					<SRLWrapper>
-						<div className="gap-1 grid-flow-row-dense auto-rows-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 cursor-pointer  ">
+						<div className="grid auto-cols-auto sm:grid-cols-2 lg:grid-cols-6 gap-5 cursor-pointer  ">
 							{imageData &&
 								imageData.map((photo, index) => (
 									<img
 										src={photo.mainImage.asset.url}
 										alt=""
-										className=" object-cover h-full  w-full "
+										className=" object-cover h-full w-full transform hover:scale-105 transition duration-700 ease-in-out"
 									/>
 								))}
 						</div>
